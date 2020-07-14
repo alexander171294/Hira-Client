fce2b68f7d8a3d046d4f3ead5d50c6f74d8e46e1
Alexander Eberle <alexander171294@gmail.com>
2020-07-14 18:22:57 -0300

    main components

--- hiraClient/src/app/app.module.ts
+++ hiraClient/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { ChatBoxComponent } from './components/chat-box/chat-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatListComponent,
+    UserListComponent,
+    ChatBoxComponent
   ],
   imports: [
     BrowserModule
