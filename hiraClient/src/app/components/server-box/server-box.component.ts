0b45896d8994e9c8f3b013702e922951a9f05ec8
Alexander Eberle <alexander171294@gmail.com>
2020-07-26 03:37:35 -0300

    updating todo

--- hiraClient/src/app/components/server-box/server-box.component.ts
+++ hiraClient/src/app/components/server-box/server-box.component.ts
@@ -8,7 +8,7 @@ import { ServerData } from 'src/app/services/ServerData';
 })
 export class ServerBoxComponent implements OnInit {
 
-  public server: string = 'irc.hira.li:6667';
+  public server: string = 'kappa.hira.li:6667';
   public apodo: string = 'Zetta007';
   public apodoSecundario: string = 'harkonidaz_tst02';
   public autojoin: string = '#harkolandia';
