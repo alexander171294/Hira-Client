bf879b8399371dcfc6dec8ff32fb049fca8dd877
Alexander Eberle <alexander171294@gmail.com>
2020-08-14 11:26:01 -0300

    cosas.

--- hiraClient/src/environments/environment.ts
+++ hiraClient/src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   electron: false,
   // gateway: 'ws://irc.network.org:8091/webirc/websocket/',
-  gateway: 'wss://thira.tandilserver.com:4433/webirc/websocket/',
+  // gateway: 'wss://server1.tandilserver.com:4433/webirc/websocket/',
+  gateway: 'wss://avalon.hira.li:7002',
   toolService: 'http://localhost:3030/',
   maxLogs: 50
 };
