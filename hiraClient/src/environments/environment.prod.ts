c364538807a7e23bfbc514fb641d0fca99522cec
Alexander Eberle <alexander171294@gmail.com>
2020-10-12 14:08:40 -0300

    comment old endpoints

--- hiraClient/src/environments/environment.prod.ts
+++ hiraClient/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   electron: false,
+  // gateway: 'wss://thira.tandilserver.com:4433/webirc/websocket/',
   gateway: 'wss://wircg.tandilserver.com/webirc/websocket/',
   toolService: 'https://thira.tandilserver.com/',
   maxLogs: 50,
