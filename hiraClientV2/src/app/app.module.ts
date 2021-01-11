b918cee69355ce5ed910f60892ec4f70abe1229c
Alexander Eberle <alexander171294@gmail.com>
2021-01-11 17:41:14 -0300

    navigation

--- hiraClientV2/src/app/app.module.ts
+++ hiraClientV2/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavComponent } from './sections/nav/nav.component';
+import { MenuComponent } from './sections/menu/menu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
