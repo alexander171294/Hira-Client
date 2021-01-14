eb3b199abf3c62c792d31bf29a764b35bf8f32fd
Alexander Eberle <alexander171294@gmail.com>
2021-01-14 13:26:58 -0300

    version

--- hiraClientV2/src/app/app.component.ts
+++ hiraClientV2/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hiraClientV2';
+  codename = environment.codename;
+  version = environment.version;
 }
--- hiraClientV2/src/environments/environment.ts
+++ hiraClientV2/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  version: '2.0.1',
+  codename: 'B612'
 };
 
 /*
