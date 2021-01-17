27b42e20adc656a41b060ce8311bc50982f2ad1c
Alexander Eberle <alexander171294@gmail.com>
2021-01-17 12:52:20 -0300

    About

--- hiraClientV2/src/app/app-routing.module.ts
+++ hiraClientV2/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'chat', loadChildren: () => import('./pages/chat/chat.module').then(m => m.ChatModule)},
   { path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)},
   { path: 'server', loadChildren: () => import('./pages/server-messages/server.module').then(m => m.ServerModule)},
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule)},
 ];
 
 @NgModule({
---
+++ hiraClientV2/src/app/pages/about/about.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  codename = environment.codename;
+  version = environment.version;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
