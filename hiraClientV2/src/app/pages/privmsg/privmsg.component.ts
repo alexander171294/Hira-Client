b21681ffb8a5dec736a19418b1f5e1696ec8d1b9
Alexander Eberle <alexander171294@gmail.com>
2021-01-22 13:48:33 -0300

    actualizamos TODO

---
+++ hiraClientV2/src/app/pages/privmsg/privmsg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privmsg',
+  templateUrl: './privmsg.component.html',
+  styleUrls: ['./privmsg.component.scss']
+})
+export class PrivmsgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
