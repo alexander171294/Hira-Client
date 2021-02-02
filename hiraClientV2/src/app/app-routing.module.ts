d22723da4ee0f1989ff46cd8730c50d200752659
Alexander Eberle <alexander171294@gmail.com>
2021-02-02 10:06:52 -0300

    whois y lista de canales

--- hiraClientV2/src/app/app-routing.module.ts
+++ hiraClientV2/src/app/app-routing.module.ts
@@ -10,6 +10,9 @@ const routes: Routes = [
   { path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)},
   { path: 'server', loadChildren: () => import('./pages/server-messages/server.module').then(m => m.ServerModule), canActivate: [ConnectedGuard]},
   { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule)},
+  { path: 'whois', loadChildren: () => import('./pages/whois/whois.module').then(m => m.WhoisModule), canActivate: [ConnectedGuard]},
+  { path: 'whois/:nick', loadChildren: () => import('./pages/whois/whois.module').then(m => m.WhoisModule), canActivate: [ConnectedGuard]},
+  { path: 'canales', loadChildren: () => import('./pages/canales/canales.module').then(m => m.CanalesModule), canActivate: [ConnectedGuard]},
 ];
 
 @NgModule({
